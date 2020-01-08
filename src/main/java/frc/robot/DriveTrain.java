dbb3eadc5d62ea6c7c616b7da980ca38b42da064
RishiAstra <40644327+RishiAstra@users.noreply.github.com>
2020-01-08 15:05:56 -0500

    afel;n;naarnj

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -1,34 +1,35 @@
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
 
 
 public class DriveTrain {
+    public static final boolean SWAP_RIGHT_LEFT_WHEEL_SIDES = false;//in case left and right are wrong (swapped), change this
+    public static final int NUMBER_OF_SPEED_CONTROLLERS = 2;
 
     public static int ctrlMode;//Joysticks = 0, GTADrive = 1, ArcadeDrive = 2
-    public static boolean SWAP_RIGHT_LEFT_WHEEL_SIDES = false;//in case left and right are wrong (swapped), change this
 
+    private boolean moveEnabled;
     private double speedMultiplier;
-    private DiffrentialDrive diffDrive;
+    private DifferentialDrive diffDrive;
     private OI input;
     private SpeedControllerGroup[] speedControllers;//even = left, odd = right
-    private SpeedControllerGroup RSpeedCtrl;
-    private SpeedControllerGroup LSpeedCtrl;
-    private Talon[] motors;
+    private WPI_TalonSRX[] motors;
 
     public DriveTrain(int[] leftMotors, int[] rightMotors, OI input, double speedMultiplier){
         this.input = input;
         this.speedMultiplier = speedMultiplier;
-        speedControllers = new SpeedControllerGroup[2];
-        diffDrive = new DiffrentialDrive();//todo
-        Talon[] tempR = new Talon[rightMotors.length];
-        Talon[] tempL = new Talon[leftMotors.length];
+        speedControllers = new SpeedControllerGroup[NUMBER_OF_SPEED_CONTROLLERS];
+        WPI_TalonSRX[] tempR = new WPI_TalonSRX[rightMotors.length];
+        WPI_TalonSRX[] tempL = new WPI_TalonSRX[leftMotors.length];
 
         for(int i = 0; i < leftMotors.length; i++){
-            tempL[i] = new Talon(leftMotors[i]);
+            tempL[i] = new WPI_TalonSRX(leftMotors[i]);
         }
         speedControllers[0] = new SpeedControllerGroup(tempL);
 
         for(int i = 0; i < rightMotors.length; i++){
-            tempR[i] = new Talon(rightMotors[i]);
+            tempR[i] = new WPI_TalonSRX(rightMotors[i]);
         }
         speedControllers[1] = new SpeedControllerGroup(tempR);
 
@@ -38,6 +39,6 @@ public class DriveTrain {
             speedControllers[0] = temp;
         }
         
-        diffDrive = new DiffrentialDrive(speedControllers[0], speedControllers[1]);//left, then right
+        diffDrive = new DifferentialDrive(speedControllers[0], speedControllers[1]);//left, then right
     }
 }
