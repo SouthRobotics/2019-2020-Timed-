d1beb60e57bb2d6b3b7470261754c57b692a7e94
RishiAstra <40644327+RishiAstra@users.noreply.github.com>
2020-01-13 15:14:39 -0500

    updated

--- src/main/java/frc/robot/DriveTrain.java
+++ src/main/java/frc/robot/DriveTrain.java
@@ -3,32 +3,27 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 //NOTES:
 /*
-moveMotors() does not "Returns true or false if movement was successful" because it is void type
-Unsure about moveMotors() with no arguments "Uses whatever the mode is to move the motors." Is a simple tankDrive() ok?
-
-Should tankDrive and gtaDrive throw errors if in wrong mode??
-
 Many of these methods should "Return false if any errors", is a try-catch required?
-
-getMotorSpeed changed to double from double[], because it does not make sence
 */
 
 public class DriveTrain {
-    public static final boolean SWAP_RIGHT_LEFT_WHEEL_SIDES = false;//in case left and right are wrong (swapped), change this
-    public static final boolean SQUARE_SPEEDS = false;//should the speeds inputted into the tankDrive be squared? this may smoothen speed or something
+    //in case left and right are wrong (swapped), change this DOES NOT CHANGE MOTOR ORDER
+    public static final boolean SWAP_RIGHT_LEFT_WHEEL_SIDES = false;
+    //should the speeds inputted into the tankDrive be squared? this may smoothen speed or something
+    public static final boolean SQUARE_SPEEDS = false;    
     public static final int NUMBER_OF_SPEED_CONTROLLERS = 2;
 
-    private int ctrlMode = 0;//Joysticks = 0, GTADrive = 1, ArcadeDrive = 2 WARNING!!! SET TO 0 BY DEFAULT
     private boolean moveEnabled = true;//can the drivetrain move at all? default is true
     private double speedMultiplier;
-    private DifferentialDrive diffDrive;
-    private OI input;
+    private DifferentialDrive diffDrive;//obj reference
+    private OI input;//obj reference
     private SpeedControllerGroup[] speedControllers;//even = left, odd = right
-    private WPI_TalonSRX[] motors;//first ones are left, then right
+    private WPI_TalonSRX[] motors;//first ones are left, then right IF NOT SWAPPED
 
     public DriveTrain(int[] leftMotors, int[] rightMotors, OI input, double speedMultiplier){
         this.input = input;
         this.speedMultiplier = speedMultiplier;
+
         //////////////////SETUP DIFFRENTIAL DRIVE, SPEEDCONTROLLERGROUPS, AND TALONS/////////////
         this.speedControllers = new SpeedControllerGroup[NUMBER_OF_SPEED_CONTROLLERS];
         this.motors = new WPI_TalonSRX[leftMotors.length + rightMotors.length];
@@ -47,7 +42,7 @@ public class DriveTrain {
         }
         speedControllers[1] = new SpeedControllerGroup(tempR[0], tempR[1], tempR[2]);
 
-        if(SWAP_RIGHT_LEFT_WHEEL_SIDES){// swap if the sides are opposite
+        if(SWAP_RIGHT_LEFT_WHEEL_SIDES){// swap if the sides are opposite DOES NOT SWAP MOTORS ARRAY ORDER
             SpeedControllerGroup temp = speedControllers[1];
             speedControllers[1] = speedControllers[0];
             speedControllers[0] = temp;
@@ -57,53 +52,88 @@ public class DriveTrain {
         //////////////////END OF MOTORS SETUP AND STUFF//////////////////////
     }
 
+    //////////////MOVEMENT////////////////
+    //Joysticks = 0, GTADrive = 1, ArcadeDrive = 2 WARNING!!! SET TO 0 BY DEFAULT
+    
+    //read OI (input) speeds and feed it into main moveMotors
     void moveMotors(){
         int[] temp = input.getSpeeds();
-        return moveMotors(temp[0], temp[1], true);
+        moveMotors(temp[0], temp[1], true);
     };
+
+    //main moveMotors, sets the motors (using DiffrentialDrive) to speeds, can use global speed multiplier or not
     void moveMotors(double leftSpeed, double rightSpeed, boolean speedMultiplier){
         //speed wil be multiplied by this.speedMultiplier if boolean speedMultiplier is true, otherwise, 1
-        if(!moveEnabled) return;
+        if(!moveEnabled) 
+            return;
         double speedMult = speedMultiplier ? this.speedMultiplier : 1;
         diffDrive.tankDrive(leftSpeed * speedMult, rightSpeed * speedMult, SQUARE_SPEEDS);
     }
-    boolean tankDrive(){
-        if(THERE_IS_NOT_XBOX){
-            moveMotors();
-            return true;
-        }else{
+    boolean checkMode(int mode){
+        try{
+            if(OI.getControlMode() == mode && moveEnabled){
+                moveMotors();
+                return true;
+            }
+            else{
+                return false;
+            }
+        }catch(Exception e){//perhaps this should print to console
             return false;
         }
     }
-    boolean gtaDrive(){
-        if(THERE_IS_NOT_XBOX){
-            return false;
-        }else{
-            moveMotors();
-            return true;
-        }
-    }
-    boolean arcadeDrive(){
-        if(ctrlMode != 2){
-            throw new Exception("Wrong mode for arcadeDrive. Mode (ctrlMode) is " + ctrlMode + " but expected 2");
-            return false;//unreachable code?? is it reachable after an Exception?
-        }
-        if(THERE_IS_NOT_XBOX){
-            moveMotors();
-            return true;
-        }else{
-            return false;
-        }
-    }
+    //basically moveMotors(); but checks if mode is right and returns if wrong mode or error
+    boolean tankDrive(){return checkMode(0);}
+    //     try{
+    //         if(OI.getControlMode() == 0 && moveEnabled){
+    //             moveMotors();
+    //             return true;
+    //         }
+    //         else{
+    //             return false;
+    //         }
+    //     }catch(Exception e){//perhaps this should print to console
+    //         return false;
+    //     }
+    // }
 
-    //danger - make sure index is right
-    double getMotorSpeed(int index){
-        return motors[index].get();
-    }
+    //basically moveMotors(); but checks if mode is right and returns if wrong mode or error
+    boolean gtaDrive(){return checkMode(1);}
+    //     try{
+    //         if(OI.getControlMode() == 1 && moveEnabled){
+    //             moveMotors();
+    //             return true;
+    //         }
+    //         else{
+    //             return false;
+    //         }
+    //     }catch(Exception e){//perhaps this should print to console
+    //         return false;
+    //     }
+        
+    // }
 
+    //basically moveMotors(); but checks if mode is right and returns if wrong mode or error
+    boolean arcadeDrive(){return checkMode(2);}
+    //     try{
+    //         if(OI.getControlMode() == 2 && moveEnabled){
+    //             moveMotors();
+    //             return true;
+    //         }
+    //         else{
+    //             return false;
+    //         }
+    //     }catch(Exception e){//perhaps this should print to console
+    //         return false;
+    //     }
+        
+    // }
+
+    //////////////////END MOVEMENT/////////////////
+    ////////////////GET////////////
+    double getMotorSpeed(int index){return motors[index].get();}
     double getLeftSpeed(){return speedControllers[0].get();}
     double getRightSpeed(){return speedControllers[1].get();}
-
+    ////////////////////SET/////////////////////
     void setEnable(boolean enable){moveEnabled = enable;}
-    void setMode(int mode){ctrlMode = mode;}
 }
