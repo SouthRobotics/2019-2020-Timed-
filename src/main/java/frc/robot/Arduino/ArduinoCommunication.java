16a50679a9bfb3d7a416b086799b5d1388ed82ab
Official Computer <39636236+SouthRobotics@users.noreply.github.com>
2020-01-23 15:21:09 -0500

    Update ArduinoCommunication.java

--- src/main/java/frc/robot/Arduino/ArduinoCommunication.java
+++ src/main/java/frc/robot/Arduino/ArduinoCommunication.java
@@ -7,15 +7,50 @@
 
 package frc.robot.Arduino;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
 
-/**
- * Add your docs here.
- */
+
 public class ArduinoCommunication {
 
-    private long baud_rate;
+    private int baud_rate;
     private SerialPort serial;
 
-    
+    public ArduinoCommunication(int baud, Port port)
+    {
+        baud_rate = baud;
+        serial = new SerialPort(baud, port);
+    }
+
+    public ArduinoCommunication(int baud)
+    {
+        baud_rate = baud;
+        serial = new SerialPort(baud, SerialPort.Port.kUSB1);
+    }
+
+    public void sendString(String data)
+    {
+        serial.writeString(data);
+    }
+
+    public String readString()
+    { //Returns a string from the serial port
+        serial.setTimeout(2);
+        return serial.readString();
+    }
+
+    public String readByte()
+    { //Reads 1 byte of the Serial Data, returns null if there is an exception thrown
+        try{
+            return new String(serial.read(1), "UTF-8");
+        }
+        catch(Exception e){}
+        
+        return null;
+    }
+
+    public int getBaudRate()
+    {
+        return baud_rate;
+    }
 
 }
