eeae20241d317529acf83c06ccadaf76c1f009a5
MaksimPeterburgskiy <maksim.peter777@gmail.com>
2020-02-24 10:44:03 -0800

    added comments to code

--- src/main/java/frc/robot/CTREMagEncoder.java
+++ src/main/java/frc/robot/CTREMagEncoder.java
@@ -16,6 +16,10 @@ import edu.wpi.first.wpilibj.DigitalInput;
  * Unless you know what you are doing, do not modify this file except to change
  * the parameter class to the startRobot call.
  */
+
+    //motor.getSelectedSensorPosition();
+    //motor.getSelectedSensorVelocity();
+    //motor.setSelectedSensorPosition(sensorPos)
 public class CTREMagEncoder  {
 
     private int zeroDistance;
@@ -27,23 +31,32 @@ public class CTREMagEncoder  {
     
 
 
-
+    /**
+     * @param motor The Talon motor the CTREMAgEncode will refernce
+     */
     public CTREMagEncoder (WPI_TalonSRX motor){
         motorAssigned = motor;
         motorAssigned.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
         zeroDistance = motor.getSelectedSensorPosition();
     }
 
+
+    /**
+     * @param motor The Talon motor the CTREMAgEncode will refernce
+     * @param feedback The Feedback device, ie encoder, that the motor will refernece
+     */
     public CTREMagEncoder (WPI_TalonSRX motor, FeedbackDevice feedback){
         motorAssigned = motor;
         motorAssigned.configSelectedFeedbackSensor(feedback);
         zeroDistance = motor.getSelectedSensorPosition();
-        
-        //motor.getSelectedSensorPosition();
-        //motor.getSelectedSensorVelocity();
-        //motor.setSelectedSensorPosition(sensorPos)
     }
 
+
+    /**
+     * @param motor The Talon motor the CTREMAgEncode will refernce
+     * @param limitSwitchPort The port for the limit switch
+     * @param switchMode1 The mode the limit switch will operate in by default. 0 - default open. 1 - default closed
+     */
     public CTREMagEncoder (WPI_TalonSRX motor, int limitSwitchPort, int switchMode1){
         motorAssigned = motor;
         motorAssigned.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative);
@@ -53,6 +66,14 @@ public class CTREMagEncoder  {
 
     }
 
+
+
+    /**
+     * @param motor The Talon motor the CTREMAgEncode will refernce
+     * @param feedback The Feedback device, ie encoder, that the motor will refernece
+     * @param limitSwitchPort The port for the limit switch
+     * @param switchMode1 The mode the limit switch will operate in by default. 0 - default open. 1 - default closed
+     */
     public CTREMagEncoder (WPI_TalonSRX motor, FeedbackDevice feedback, int limitSwitchPort, int switchMode1){
         motorAssigned = motor;
         motorAssigned.configSelectedFeedbackSensor(feedback);
@@ -62,22 +83,42 @@ public class CTREMagEncoder  {
 
     }
     
+
+    /**
+     * @return Returns the current raw speed of the motor associated with the MagEncoder
+     */
     public int getSpeed(){
         return motorAssigned.getSelectedSensorVelocity();
     }
 
+
+    /**
+     * @return Returns the current raw encoder position of the encooder associated with the Motor
+     */
     public int getRawPosition(){
         return motorAssigned.getSelectedSensorPosition();
     }
 
+
+    /**
+     * @return Returns the current encoder position minus the zero distance of the encooder associated with the Motor
+     */
     public int getPosition(){
         return motorAssigned.getSelectedSensorPosition() - zeroDistance;
     }
 
+
+    /**
+     * @return Sets the zeroDistance to the current position of the encoder
+     */
     public void setCurrentPositionZero(){
         zeroDistance = motorAssigned.getSelectedSensorPosition();
     }
 
+
+    /**
+     * @return Returns if a ceratin limit switch is "activated" depending on the limitswitch port and mode specified in the constructor.
+     */
     public boolean checkLimitSwitchPeriodic(){
         boolean returnVal = false;
         
@@ -91,8 +132,5 @@ public class CTREMagEncoder  {
         }
         return returnVal;
     }
-    
-
-
 
 }
